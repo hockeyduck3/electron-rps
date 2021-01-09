40533be5779f50cb8a1412b9dec85eb00dab5390
LJ Spencer <hockeyduck3@gmail.com>
2021-01-09 15:25:41 -0700

    Added a components folder, and mad some changes to App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import './App.css';
+import React from 'react';
+import { HashRouter as Router } from 'react-router-dom';
+
+import TitleBar from './components/titleBar/index'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <TitleBar />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/titleBar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function TitleBar() {
+    return (
+        <nav>
+            <div id='close'>X</div>
+            <div id='max'></div>
+            <div id='min'>-</div>
+        </nav>
+    )
+}
+
+export default TitleBar;
