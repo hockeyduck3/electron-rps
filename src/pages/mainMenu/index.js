c1b3903225bca96947e473d2f45ccaaeea0fb778
LJ Spencer <hockeyduck3@gmail.com>
2021-01-10 16:06:20 -0700

    Changed the background color, switched out the photos to paper and sci

--- src/pages/mainMenu/index.js
+++ src/pages/mainMenu/index.js
@@ -6,8 +6,8 @@ function MainMenu() {
     return (
         <div className='grid grid-rows-1 grid-flow-row grid-cols-3 mt-32 gap-10'>
             <img src='../../img/rock.png' alt='Rock' className='animate__animated animate__bounceInDown mx-auto w-10/12'/>
-            <img src='../../img/paper.png' alt='Paper' className='animate__animated animate__bounceInDown animate__delay-1s mx-auto w-7/12'/>
-            <img src='../../img/scissors.png' alt='Scissors' className='animate__animated animate__bounceInDown animate__delay-2s mx-auto w-7/12'/>
+            <img src='../../img/paper.png' alt='Paper' className='animate__animated animate__bounceInDown animate__delay-1s mx-auto w-8/12'/>
+            <img src='../../img/scissors.png' alt='Scissors' className='animate__animated animate__bounceInDown animate__delay-2s mx-auto w-8/12 align-top'/>
         </div>
     )
 }
