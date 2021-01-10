dfa44180084421b68add728f77b8a846a98a9470
LJ Spencer <hockeyduck3@gmail.com>
2021-01-10 12:58:17 -0700

    Added an image folder, and started adding those images to the main menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { HashRouter as Router } from 'react-router-dom';
 
-import TitleBar from './components/titleBar/index'
+import TitleBar from './components/titleBar/index';
+import MainMenu from './pages/mainMenu/index';
 
 function App() {
   return (
     <Router>
       <div>
         <TitleBar />
+        <MainMenu />
       </div>
     </Router>
   );
---
+++ src/pages/mainMenu/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import 'animate.css';
+
+function MainMenu() {
+    return (
+        <div className='grid grid-rows-1 grid-flow-row grid-cols-3 mt-32 gap-10'>
+            <img src='../../img/rock.png' alt='Rock' className='animate__animated animate__bounceInDown mx-auto w-10/12'/>
+            <img src='../../img/paper.png' alt='Paper' className='animate__animated animate__bounceInDown animate__delay-1s mx-auto w-7/12'/>
+            <img src='../../img/scissors.png' alt='Scissors' className='animate__animated animate__bounceInDown animate__delay-2s mx-auto w-7/12'/>
+        </div>
+    )
+}
+
+export default MainMenu;
