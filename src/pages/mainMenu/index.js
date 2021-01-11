6d19a0b5262b917643bd7530f8ee13f5b1ae97f1
LJ Spencer <hockeyduck3@gmail.com>
2021-01-10 17:09:37 -0700

    Added a title card for the main menu

--- src/pages/mainMenu/index.js
+++ src/pages/mainMenu/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
+import './style.css';
 import 'animate.css';
 
 function MainMenu() {
     return (
-        <div className='grid grid-rows-1 grid-flow-row grid-cols-3 mt-32 gap-10'>
-            <img src='../../img/rock.png' alt='Rock' className='animate__animated animate__bounceInDown mx-auto w-10/12'/>
-            <img src='../../img/paper.png' alt='Paper' className='animate__animated animate__bounceInDown animate__delay-1s mx-auto w-8/12'/>
-            <img src='../../img/scissors.png' alt='Scissors' className='animate__animated animate__bounceInDown animate__delay-2s mx-auto w-8/12 align-top'/>
+        <div>
+            <div id='titleGroup' className='animate__animated animate__bounceIn animate__delay-3s grid grid-rows-1 grid-flow-row grid-cols-3 mt-10'>
+                <h1 id='rock' className=' text-gray-300 text-7xl mt-2'>Rock</h1>
+                <h1 id='paper' className='text-white text-7xl text-center mt-2'>Paper</h1>
+                <h1 id='scissors' className='text-red-600 text-6xl text-center mt-2'>Scissors</h1>
+            </div>
+
+            <div className='grid grid-rows-1 grid-flow-row grid-cols-3 mt-20 gap-10'>
+                <img src='../../img/rock.png' alt='Rock' className='animate__animated animate__bounceInDown mx-auto w-10/12'/>
+                <img src='../../img/paper.png' alt='Paper' className='animate__animated animate__bounceInDown animate__delay-1s mx-auto w-8/12'/>
+                <img src='../../img/scissors.png' alt='Scissors' className='animate__animated animate__bounceInDown animate__delay-2s mx-auto w-8/12 align-top'/>
+            </div>
         </div>
     )
 }
